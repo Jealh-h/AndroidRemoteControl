a005a39e0eb7451f0d9cfcdcd7295d1aa71a0252
jealh <1620175472@qq.com>
2023-03-25 21:21:03 +0800

    first commit

---
+++ app/src/main/java/com/example/javaremotecontroller/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.javaremotecontroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.show_count);
+        Log.d("MainActivity", "Hello world");
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        count++;
+        if(mTextView!=null){
+            mTextView.setText(Integer.toString(count));
+        }
+    }
+}
